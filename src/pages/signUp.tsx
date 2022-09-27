a319a4877f39a3b3d4d23576554e74a73049627a
Razvan <Razvanbeldeanu789@gmail.com>
2022-09-27 12:14:52 -0400

    Added a router section to reset password to provide room for email input

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -6,9 +6,12 @@ import { DatePicker } from '@mui/x-date-pickers'
 import { useState } from 'react'
 import dayjs, { Dayjs } from 'dayjs';
 import Link from 'next/link'
+import useWindowDimensions from '../utils/windowControl'
+
 export default function SignUp() {
     const [cardDetailsOpen, setCardDetailsOpen] = useState(false);
     const [expirationDate, setExpirationDate] = useState<Dayjs | null>(null);
+    const screen = useWindowDimensions();
 
     function handleOpenCard() {
         if (cardDetailsOpen) {
@@ -24,35 +27,40 @@ export default function SignUp() {
                 <h1 className="w-full text-center text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-600 ">
                     <span className="text-purple-300">Sign</span> Up
                 </h1>
-                <form className='flex flex-col space-y-6 p-4'>
-                    <TextField variant='standard' type="email" label='Email'></TextField>
-                    <TextField variant='standard' type="text" label='Username'></TextField>
-                    <TextField variant='standard' type="password" label='Password'></TextField>
-                    <TextField variant='standard' type="password" label='Confirm Password'></TextField>
-
-                    <Button onClick={handleOpenCard}> {cardDetailsOpen ? "Not Right Now" : "Add a Payment Method"}</Button>
-                </form>
-                <div className={cardDetailsOpen ? "block" : "hidden"}>
-                    <Divider />
-                    <form className='flex flex-col space-y-6 p-4 mb-4'>
-                        <TextField variant='standard' type="text" label='Cardholder Name'></TextField>
-                        <TextField variant='standard' type="text" label='Card Number'></TextField>
-                        <DatePicker views={['year', 'month']}
-                            label="Year and Month"
-                            minDate={dayjs('2012-03-01')}
-                            maxDate={dayjs('2023-06-01')}
-                            value={expirationDate}
-                            onChange={(newValue) => {
-                                setExpirationDate(newValue);
-                            }}
-                            renderInput={(params) => <TextField {...params} variant='standard' helperText={null} />}
-                        />
-                        <TextField variant='standard' type="text" label='CCV'></TextField>
+                <div className="lg:flex">
+                    <form className='flex flex-col space-y-6 p-4 mb-4 w-full'>
+                        <TextField variant='standard' type="email" label='Email'></TextField>
+                        <TextField variant='standard' type="text" label='Username'></TextField>
+                        <TextField variant='standard' type="password" label='Password'></TextField>
+                        <TextField variant='standard' type="password" label='Confirm Password'></TextField>
                     </form>
+                    <div className={cardDetailsOpen ? "block w-full" : "hidden"}>
+                        {/* <Divider className='lg:float-left' orientation={
+                            screen?.width >= 1024 ? "vertical" : "horizontal"
+                            } /> */}
+                        <form className='flex flex-col space-y-6 p-4 mb-4'>
+                            <TextField variant='standard' type="text" label='Cardholder Name'></TextField>
+                            <TextField variant='standard' type="text" label='Card Number'></TextField>
+                            <DatePicker views={['year', 'month']}
+                                label="Year and Month"
+                                minDate={dayjs()}
+                                maxDate={dayjs().add(10, 'year')}
+                                value={expirationDate}
+                                onChange={(newValue) => {
+                                    setExpirationDate(newValue);
+                                }}
+                                renderInput={(params) => <TextField {...params} variant='standard' helperText={null} />}
+                            />
+                            <TextField variant='standard' type="text" label='CCV'></TextField>
+                        </form>
+                    </div>
                 </div>
+                <Button className='w-full my-2' onClick={handleOpenCard}> {cardDetailsOpen ? "Not Right Now" : "Add a Payment Method"}</Button>
+
                 <Link href='/login'>
                     <Button className="bg-purple-300 w-full font-extrabold" variant='contained'>Sign Up</Button>
                 </Link>
+                <Link href='/login'><p className='text-purple-300 cursor-pointer text-center mt-3 underline'>Return to Login</p></Link>
             </StyleBox>
         </Layout>
     )
