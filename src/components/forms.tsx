88fe6cef405408464fdfaad11aa290f806b74c02
Lindsey Andrews <lga56672@uga.edu>
2022-10-25 23:24:48 -0400

    added most of the functionality for the email verification, though just short of fully functional

--- src/components/forms.tsx
+++ src/components/forms.tsx
@@ -130,9 +130,7 @@ export function SignUpForm() {
                             <TextField name="phoneNumber" variant='standard' type="text" label='Phone Number *' value={signUpInfo.phoneNumber} onChange={handleSignUpChange}></TextField>
                             <TextField name="password" variant='standard' type="password" label='Password *' onChange={handleSignUpChange}></TextField>
                             <TextField name="confirmPassword" variant='standard' type="password" label='Confirm Password *' onChange={handleSignUpChange}></TextField>
-                            {/* /* var bcrypt = require('bcryptjs');
-                            var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync("B4c0/\/", salt); */ }
+                            
                         </div>
 
                         <div id='botField' className={cardDetailsOpen ? "block w-full" : "hidden"}>
--- src/utils/encryption.tsx
+++ src/utils/encryption.tsx
@@ -1,7 +1,7 @@
 export function encrypt(text: string, key: string): string {
     var bcrypt = require('bcryptjs');
     var hash = bcrypt.hashSync(text, key);
-    console.log(hash);
+    //console.log(hash);
     return hash;
 }
 
