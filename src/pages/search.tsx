c0c680ea82969f36460203d540c95288181985f7
Razvan <Razvanbeldeanu789@gmail.com>
2022-11-10 11:44:04 -0500

    Refactored skeleton code

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../components/layout'
-import { CircularProgress, Skeleton } from '@mui/material'
 import { useRouter } from 'next/router'
+import Card from '../components/card'
+import CardSkeletons from '../components/cardSkeletons'
 
 export default function Search() {
     const [loading, setLoading] = useState(true)
     const router = useRouter();
     const searchTerm = router.query.term;
-    
+
     setTimeout(() => {
         setLoading(false);
     }, 3000)
@@ -15,27 +16,20 @@ export default function Search() {
     useEffect(() => {
         setLoading(true);
     }, [searchTerm])
-    
+
     return (
         <Layout>
             <h1 className='font-extrabold text-4xl text-center pt-4 text-text-dark'><span className='text-primary'>Search</span> Results: </h1>
 
-            {loading ? (
-                <div className='flex justify-center items-center flex-wrap mt-10 space-x-5 space-y-5'>
-                    <Skeleton variant="rectangular" className="rounded-xl ml-5 mt-5" width={210} height={300} />
-                    <Skeleton variant="rectangular" className="rounded-xl" width={210} height={300} />
-                    <Skeleton variant="rectangular" className="rounded-xl" width={210} height={300} />
-                    <Skeleton variant="rectangular" className="rounded-xl" width={210} height={300} />
-                    <Skeleton variant="rectangular" className="rounded-xl" width={210} height={300} />
-                    <Skeleton variant="rectangular" className="rounded-xl" width={210} height={300} />
-                    <Skeleton variant="rectangular" className="rounded-xl" width={210} height={300} />
-                    <Skeleton variant="rectangular" className="rounded-xl" width={210} height={300} />
-                </div>) : (
-                <div className='flex justify-center items-center flex-wrap mt-10 space-x-5 space-y-5'>
-                    <div className="w-60 h-96 rounded-xl ml-5 mt-5 bg-primary"></div>
-                    <div className="w-60 h-96 rounded-xl bg-primary"></div>
-                </div>
-            )
+            {loading ?
+                // If loading is true, show skeletons
+                <CardSkeletons cardCount={5} />
+                : //If loading is false, show cards 
+                (
+                    <div className='flex justify-center items-center flex-wrap mt-10 space-x-5 space-y-5'>
+                        <Card />
+                    </div>
+                )
             }
         </Layout>
 
