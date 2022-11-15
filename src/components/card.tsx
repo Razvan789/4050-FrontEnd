2082256db740e657e6352c6f1c4ec1dc37543edf
Razvan789 <razvanbeldeanu789@gmail.com>
2022-11-14 22:02:02 -0500

    Index now pulls from data base and so does book ticket

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -10,14 +10,26 @@ interface CardProps {
     little?: boolean;
 }
 
+const defaultMovie: Movie = {
+    id: 0,
+    title: "",
+    cast: "",
+    director: "",
+    producer: "",
+    synopsis: "",
+    reviews: "",
+    ratingCode: 0,
+    trailerPic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu5iprMc1MEpOTSC7jAnweLJdcMeUL3jJg4AiNoTs&s",
+    video: ""    
+}
 
-export default function Card({ movie = getMovie(1), link = "/BookTicket/1", little = false }: CardProps) {
+export default function Card({ movie , link = "/BookTicket/1", little = false }: CardProps) {
     const cardSizeClasses: string = little ? " hover:scale-105 w-1/6 md:w-1/5 lg:w-1/6 xl:w-1/7" : "hover:scale-110 w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6";
     return (
         <Link href={link}>
             <div className={'relative min-w-[200px] m-2 group cursor-pointer rounded-lg  shadow-md hover:shadow-lg transition-transform ' + cardSizeClasses}>
-                <Image src={movie.trailerPic}
-                    alt={movie.title}
+                <Image src={movie?.trailerPic || defaultMovie.trailerPic}
+                    alt={movie?.title}
                     loader={myLoader}
                     width={350}
                     height={500}
@@ -26,9 +38,9 @@ export default function Card({ movie = getMovie(1), link = "/BookTicket/1", litt
                 {little ? null
                     :
                     <div id="cardDesc" className='z-10 md:-z-10 absolute w-full bottom-0 rounded-b-lg bg-bg-dark opacity-100 p-3 md:group-hover:translate-y-full transition-all overflow-visible'>
-                        <h1 className='text-lg font-extrabold text-text-dark'>{movie.title}</h1>
+                        <h1 className='text-lg font-extrabold text-text-dark'>{movie?.title}</h1>
                         <div className="flex flex-col items-center justify-center">
-                            <Rating name="read-only" value={movie.ratingCode} precision={0.1} readOnly />
+                            <Rating name="read-only" value={movie?.ratingCode} precision={0.1} readOnly />
                         </div>
                     </div>
                 }
--- src/utils/movie.tsx
+++ src/utils/movie.tsx
@@ -1,3 +1,4 @@
+import { serverUrl } from "./backendInfo";
 
 export type Movie = {
     id: number;
@@ -13,10 +14,7 @@ export type Movie = {
 };
 
 
-//Gets all movies from the database
-export function getMovies(): Movie[] {
-    return [];
-}
+
 
 //Gets range of movies from the database
 export function getMoviesRange(start: number, end: number): Movie[] {
@@ -24,17 +22,32 @@ export function getMoviesRange(start: number, end: number): Movie[] {
 }
 
 
-export function getMovie(id: number): Movie {
-    return ({
-        id: id,
-        title: "Spider-Man No Way Home",
-        cast: "Tom Holland, Zendaya, Benedict Cumberbatch, Jacob Batalon, Marisa Tomei, Jamie Foxx, Alfred Molina, Toby Maguire, Andrew Garfield",
-        director: "Jon Watts",
-        producer: "Kevin Feige",
-        synopsis: "Peter Parker's relaxing European vacation takes an unexpected turn when Nick Fury shows up in his hotel room to recruit him for a mission. The world is in danger as four massive elemental creatures, known as the Elementals, emerge from an alternate dimension. To save the planet and humanity, Parker and his friends must embark on a dangerous journey to another dimension to fight them. Parker's worst fears soon become reality when Mysterio appears to be one of the evil entities and kills Fury. Parker then realizes that all of his enemies will join forces to destroy him.",
-        reviews: "Good movie",
-        ratingCode: 3.5,
-        trailerPic: "https://images.fandango.com/ImageRenderer/0/0/redesign/static/img/default_poster.png/0/images/masterrepository/Fandango/225415/SMRR_935x1381_Digital_1Sheet.jpg",
-        video: "https://www.youtube.com/watch?v=QwievZ1Tx-8"
+export async function getMovie(id: number): Promise<Movie> {
+    return new Promise((resolve, reject) => {
+        fetch(`${serverUrl}/movie?movieID=${id}`).then(res => res.json()).then(data => {
+            resolve(data as Movie);
+        }).catch(err => {
+            reject(err);
+        })
+    });
+    // return ({
+    //     id: id,
+    //     title: "Spider-Man No Way Home",
+    //     cast: "Tom Holland, Zendaya, Benedict Cumberbatch, Jacob Batalon, Marisa Tomei, Jamie Foxx, Alfred Molina, Toby Maguire, Andrew Garfield",
+    //     director: "Jon Watts",
+    //     producer: "Kevin Feige",
+    //     synopsis: "Peter Parker's relaxing European vacation takes an unexpected turn when Nick Fury shows up in his hotel room to recruit him for a mission. The world is in danger as four massive elemental creatures, known as the Elementals, emerge from an alternate dimension. To save the planet and humanity, Parker and his friends must embark on a dangerous journey to another dimension to fight them. Parker's worst fears soon become reality when Mysterio appears to be one of the evil entities and kills Fury. Parker then realizes that all of his enemies will join forces to destroy him.",
+    //     reviews: "Good movie",
+    //     ratingCode: 3.5,
+    //     trailerPic: "https://images.fandango.com/ImageRenderer/0/0/redesign/static/img/default_poster.png/0/images/masterrepository/Fandango/225415/SMRR_935x1381_Digital_1Sheet.jpg",
+    //     video: "https://www.youtube.com/watch?v=QwievZ1Tx-8"
+    // });
+}
+
+export async function getAllMovies(): Promise<Movie[]> {
+    return new Promise<Movie[]>(resolve => {
+        fetch(`${serverUrl}/movie`).then(res => res.json()).then(data => {
+            resolve(data as Movie[]);
+        });
     });
 }
